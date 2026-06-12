14df56b78ddf24e26cd1019fc1a616c26a5270e1
Moiz Juzer Siamwala <114101927+MrIT07@users.noreply.github.com>
2026-06-12 15:03:41 +0530

    Added first GenAI project - Text-to-Image Generator using Stable Diffusion and Gradio

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+from diffusers import StableDiffusionPipeline
+from huggingface_hub import login
+import torch
+import gradio as gr
 
+# Install dependencies
+# pip install transformers diffusers torch pillow gradio
+
+# Login to Hugging Face
+login()
+
+# Load model
+model_id = "runwayml/stable-diffusion-v1-5"
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16
+)
+
+pipe.to("cuda")
+
+def generate_image(prompt):
+    image = pipe(prompt).images[0]
+    return image
+
+interface = gr.Interface(
+    fn=generate_image,
+    inputs=gr.Textbox(label="Enter Image Description"),
+    outputs=gr.Image(label="Generated Image"),
+    title="Gen AI Text-to-Image App",
+    description="Enter a descriptive text to generate an image."
+)
+
+interface.launch(share=True, debug=True)
